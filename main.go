8545e904ad19ce685738057d846ce3329e363ff1
Wessrow <gurra_gla@hotmail.com>
2024-09-27 14:06:44 +0200

    some works

--- main.go
+++ main.go
@@ -5,49 +5,49 @@ import (
 	"fmt"
 	"go-ntc-templates/models"
 	"os"
+	"strings"
 
 	"github.com/melbahja/goph"
 	"github.com/sirikothe/gotextfsm"
 	"github.com/sirupsen/logrus"
 )
 
-func testSsh(template string) models.Cisco_ios_show_version {
-
-	fsm := gotextfsm.TextFSM{}
-	err := fsm.ParseString(string(template))
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	client, err := goph.New("admin", "10.0.100.208", goph.Password("Netw0rking!"))
-	if err != nil {
-		logrus.Error(err)
-	}
+func testSsh(client *goph.Client, command string) string {
 
 	// Defer closing the network connection.
 	defer client.Close()
 
 	// Execute your command.
-	out, err := client.Run("show version")
+	out, err := client.Run(command)
 
 	if err != nil {
 		logrus.Fatal(err)
 	}
 
+	return string(out)
+}
+
+func parseOutput(input string, template string) any {
+
+	fsm := gotextfsm.TextFSM{}
+	err := fsm.ParseString(template)
+	if err != nil {
+		logrus.Error(err)
+	}
 	parser := gotextfsm.ParserOutput{}
-	err = parser.ParseTextString(string(out), fsm, true)
+	err = parser.ParseTextString(string(input), fsm, true)
 	if err != nil {
 		logrus.Error(err)
 	}
 
-	str, err := json.Marshal(parser.Dict[0])
+	str, err := json.Marshal(parser.Dict)
 	if err != nil {
 		fmt.Println("Error during marshaling:", err)
 	}
 	fmt.Println("Marshalled JSON:", string(str))
 
 	// Unmarshal into the struct
-	model := models.Cisco_ios_show_version{}
+	model := []models.Cisco_ios_show_version{}
 	err = json.Unmarshal(str, &model)
 	if err != nil {
 		fmt.Println("Error during unmarshaling:", err)
@@ -58,12 +58,23 @@ func testSsh(template string) models.Cisco_ios_show_version {
 
 func main() {
 
-	template, err := os.ReadFile("./templates/cisco_ios_show_version.textfsm")
+	command := "show version"
+
+	template, err := os.ReadFile("./templates/cisco_ios_" + strings.ReplaceAll(command, " ", "_") + ".textfsm")
 	if err != nil {
 		logrus.Error(err)
 	}
-	test := testSsh(string(template))
-	logrus.Info(test.MAC_ADDRESS)
-	// parse.ParseFSM("cisco_ios_show_version", string(template))
+
+	client, err := goph.New("admin", "10.0.100.208", goph.Password("Netw0rking!"))
+	if err != nil {
+		logrus.Error(err)
+	}
+	commandReturn := testSsh(client, command)
+
+	parsedCommand := parseOutput(commandReturn, string(template))
+
+	logrus.Info(parsedCommand)
+
+	//parse.ParseFSM("cisco_ios_show_interfaces", string(template))
 
 }
--- parse/parse.go
+++ parse/parse.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type CidrResponse struct {
@@ -17,6 +18,20 @@ func check(e error) {
 	}
 }
 
+func capitalizeFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s // return the original string if it's empty
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+func capitalizeFirstLetterLowerRest(s string) string {
+	if len(s) == 0 {
+		return s // return the original string if it's empty
+	}
+	return strings.ToUpper(string(s[0])) + strings.ToLower(s[1:])
+}
+
 func ParseFSM(name string, template string) {
 	// scanner := bufio.NewScanner(strings.NewReader(template))
 	// for scanner.Scan() {
@@ -27,23 +42,23 @@ func ParseFSM(name string, template string) {
 	// 	}
 	// }
 
-	r, _ := regexp.Compile(`Value\s(List)?\s?(\w+)`)
+	r, _ := regexp.Compile(`Value\s(List|Required)?\s?(\w+)`)
 	entries := r.FindAllStringSubmatch(template, -1)
 
 	pwd, _ := os.Getwd()
-	f, err := os.Create(pwd + "/models/test.go")
+	f, err := os.Create(pwd + "/models/" + name + ".go")
 
 	check(err)
 	defer f.Close()
 
 	f.WriteString("package models\n\n")
-	f.WriteString(fmt.Sprintf("type %v struct {\n", name))
+	f.WriteString(fmt.Sprintf("type %v struct {\n", capitalizeFirstLetter(name)))
 	for _, entry := range entries {
 		if entry[1] == "List" {
-			f.WriteString(fmt.Sprintf("\t%v\t[]string\t`json:\"%v\"`\n", entry[2], entry[2]))
+			f.WriteString(fmt.Sprintf("\t%v\t[]string\t`json:\"%v\"`\n", capitalizeFirstLetterLowerRest(entry[2]), entry[2]))
 			continue
 		}
-		f.WriteString(fmt.Sprintf("\t%v\tstring\t`json:\"%v\"`\n", entry[2], entry[2]))
+		f.WriteString(fmt.Sprintf("\t%v\tstring\t`json:\"%v\"`\n", capitalizeFirstLetterLowerRest(entry[2]), entry[2]))
 	}
 	f.WriteString("}")
 
